cf18d7d4105d7846dc5c4921cfd08cd2411afb19
Thabiso317 <maekothabiso@gmail.com>
2022-09-16 06:14:48 -0700

    first commit

--- App.js
+++ App.js
@@ -1,13 +1,122 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar'
+import React ,{useState}from 'react'
+import {StyleSheet, Text, View, TouchableOpacity, ImageBackground} from 'react-native'
+import {Camera} from 'expo-camera'
+
+
+
+const CameraPreview = ({photo}) => {
+  console.log('sdsfds', photo) 
+  return (
+    <View
+      style={{
+        backgroundColor: 'transparent',
+        flex: 1,
+        width: '100%',
+        height: '100%'
+      }}
+    >
+      <ImageBackground
+        source={{uri: photo && photo.uri}}   
+        style={{
+          flex: 1
+        }}
+      />
+    </View>
+  )
+}
+
+
+
+
+
 
 export default function App() {
+  const [startCamera,setStartCamera] = React.useState(false)
+  const [previewVisible, setPreviewVisible] = useState(false)
+  const [capturedImage, setCapturedImage] = useState(null)
+  
+  //function to start camera
+   const __startCamera = async () => {
+    const {status} = await Camera.requestPermissionsAsync()
+    if (status === 'granted') {
+      // start the camera
+      setStartCamera(true)
+    } else {
+     console.log('access denied')
+    }
+  }
+
+  //function to capture
+  const __takePicture = async () => {
+    if (!camera) return
+    const photo = await camera.takePictureAsync()
+    console.log(photo)
+    setPreviewVisible(true)
+    setCapturedImage(photo)
+  }
+
+  //FUNCTION TO RETAKE A PICTURE
+  const __retakePicture = () => {
+    setCapturedImage(null)
+    setPreviewVisible(false)
+    __startCamera()
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+   <>
+    {previewVisible && capturedImage ? (
+           <CameraPreview photo={capturedImage}  retakePicture={__retakePicture} />
+          ) : (
+            <Camera
+              style={{flex: 1}}
+              ref={(r) => {
+                camera = r
+              }}
+            >
+              <View
+                style={{
+                  flex: 1,
+                  width: '100%',
+                  backgroundColor: 'transparent',
+                  flexDirection: 'row'
+                }}
+              >
+                <View
+                  style={{
+                    position: 'absolute',
+                    bottom: 0,
+                    flexDirection: 'row',
+                    flex: 1,
+                    width: '100%',
+                    padding: 20,
+                    justifyContent: 'space-between'
+                  }}
+                >
+                  <View
+                    style={{
+                      alignSelf: 'center',
+                      flex: 1,
+                      alignItems: 'center'
+                    }}
+                  >
+                    <TouchableOpacity
+                      onPress={__takePicture}
+                      style={{
+                        width: 70,
+                        height: 70,
+                        bottom: 0,
+                        borderRadius: 50,
+                        backgroundColor: '#fff'
+                      }}
+                    />
+                  </View>
+                </View>
+              </View>
+            </Camera>
+          )}
+   </>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -15,6 +124,12 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+  }
+})
+
+
+
+
+
+
